ac139da93c70be2d8d8056d50a21c030bf815df9
im-minion <vaibhavminiyar@gmail.com>
2017-09-25 00:43:13 +0530

    Removed files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MembersComponent } from './members/members.component';
-import { ProductComponent } from './product/product.component';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MembersComponent,
-    ProductComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
